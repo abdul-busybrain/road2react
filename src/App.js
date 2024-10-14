4e619799575cb0140521bda2ea7b40d07284087d
Abdullahi(busybrain) <awwalhajine@gmail.com>
2024-10-14 08:17:59 +0100

    redo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-import { type } from "@testing-library/user-event/dist/type";
-import { useEffect, useRef, useState } from "react";
-
-const useStorageState = (key, initialState) => {
-  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
-
-  useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
-
-  return [value, setValue];
-};
-
-const initialStories = [
+const list = [
   {
     title: "React",
     url: "https://reactjs.org/",
@@ -30,148 +17,34 @@ const initialStories = [
   },
 ];
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-  );
-
-const storiesReducer = (state, action) => {
-  switch (action.type) {
-    case "SET_STORIES":
-      return action.payload;
-    case "REMOVE_STORY":
-      return state.filter(
-        (story) => action.payload.objectID !== story.objectID
-      );
-    default:
-      throw new Error();
-  }
-};
-
 export default function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [stories, dispatchStories] = useState(storiesReducer, []);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    getAsyncStories()
-      .then((result) => {
-        dispatchStories({ type: "SET_STORIES", payload: result.data.stories });
-        setIsLoading(false);
-      })
-      .catch(() => setIsError(true));
-  }, []);
-
-  const handleRemoveStory = (item) => {
-    dispatchStories({
-      type: "REMOVE_STORY",
-      payload: item,
-    });
-  };
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
   return (
-    <>
-      <div>
-        <h1>My Hacker Stories</h1>
+    <div>
+      <h1>My Hacker Stories</h1>
 
-        <InputWithLabel
-          id="search"
-          label="Search"
-          value={searchTerm}
-          isFocused
-          onInputChange={handleSearch}
-        >
-          <strong> Search:</strong>
-        </InputWithLabel>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
 
-        <hr />
+      <hr />
 
-        {isError && <Error />}
-
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <List list={searchedStories} onRemoveItem={handleRemoveStory} />
-        )}
-      </div>
-    </>
+      <List />
+    </div>
   );
 }
 
-function Loader() {
-  return <p>Loading...</p>;
-}
-
-function Error() {
-  return <p>Something went wrong...</p>;
-}
-
-const InputWithLabel = ({
-  id,
-  value,
-  type = "text",
-  onInputChange,
-  isFocused,
-  children,
-}) => {
-  // A
-  const inputRef = useRef();
-
-  // C
-  useEffect(() => {
-    if (isFocused && inputRef.current) {
-      // D
-      inputRef.current.focus();
-    }
-  }, [isFocused]);
-
-  return (
-    <>
-      <label htmlFor={id}>{children}</label>&nbsp;
-      {/* B */}
-      <input
-        ref={inputRef}
-        id={id}
-        type={type}
-        value={value}
-        autoFocus={isFocused}
-        onChange={onInputChange}
-      />
-    </>
-  );
-};
-
-const List = ({ list, onRemoveItem }) => {
+function List() {
   return (
     <ul>
       {list.map((item) => (
-        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
       ))}
     </ul>
   );
-};
-const Item = ({ item, onRemoveItem }) => (
-  <li>
-    <span>
-      <a href={item.url}> {item.title}</a>
-    </span>
-    <span> {item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
-        Dismiss
-      </button>
-    </span>
-  </li>
-);
+}
