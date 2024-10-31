02240b5e5b8fb771772b3b706882875ee7126d30
Abdullahi(busybrain) <awwalhajine@gmail.com>
2024-10-31 06:38:07 +0100

    Data Fetching with React

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,9 @@ const useStorageState = (key, initialState) => {
   }, [value, key]);
   return [value, setValue];
 };
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
 
@@ -75,11 +78,12 @@ const App = () => {
   useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
